6be67751840335bf558ff92352eadf452112c527
work2544 <work21_123@hotmail.com>
2020-12-25 17:05:57 +0700

    complete

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,52 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
+void MYchat(string name){
+    cout << name << ": ";
+}
+
+int main(){
+    string username,movie,dapp,ans;
+    int id;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?";
+    cout << "\n?????: ";
+    getline(cin,username);
+    cout << "Fahsai: Wow!!! "<< username <<" is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    MYchat(username);
+    cin >> id;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR "<< (id/10000000)-12  <<". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    MYchat(username);
+    getline(cin,movie);
+
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    MYchat(username);
+    getline(cin,dapp);
+
+    cout << "Fahsai: "<< dapp <<"....that is OK!!! I'm looking forward to watching "<< movie <<" with you.\n";
+    MYchat(username);
+    getline(cin,ans);
+
+    cout << "Fahsai: 555+ see you "<< dapp <<". Bye Bye \\(^ ^)/";
+    return 0;
+}
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
+?????: Prayuth-chan
+Fahsai: Wow!!! Prayuth-chan is a really cool name.
 Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
+Prayuth-chan: 550610789
+Fahsai: I think you may be GEAR 43. I have a free movie ticket for you.
 Fahsai: Let's go to the cinema together!!!
 Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
+Prayuth-chan: Demon slayer
 Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
+Prayuth-chan: Friday the 13th
+Fahsai: Friday the 13th....that is OK!!! I'm looking forward to watching Demon slayer with you.
+Prayuth-chan: We will do as we promised, it’ll take not too long...
+Fahsai: 555+ see you Friday the 13th. Bye Bye \(^ ^)/
 */
