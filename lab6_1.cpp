950b40b5ed53b120a099e3c57c0998ced7d45bf0
work2544 <work21_123@hotmail.com>
2020-12-25 15:31:41 +0700

    complete

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
+    int a  =  20, b = 30;
+    int c = 4; float output;
+    output = a - (float)b/c;
+    cout << "output: " << output <<"\n";
     return 0;
 }
